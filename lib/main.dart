e0c8c6716a640bf69aa871df20bac87133da579c
reiji.tomooka <tomooka.04172@gmail.com>
2023-02-17 23:12:34 +0900

    以下を追加 ・Bluetooth機器との接続処理 ・LEDのON/OFF信号の送信処理

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -15,6 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(title: 'Flutter Bluetooth Serial Demo'),
     );
   }
@@ -85,8 +89,8 @@ class MyHomePage extends HookWidget {
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
             child: Text("Bonded Devices"),
           ),
           ListView.builder(
@@ -95,21 +99,128 @@ class MyHomePage extends HookWidget {
             itemBuilder: (context, int index) {
               BluetoothDevice device = bondedDevices.value.elementAt(index);
               return Card(
-                child: ExpansionTile(
+                child: ListTile(
                   title: Text(device.name ?? ""),
                   subtitle: Text(device.address),
-                  expandedAlignment: Alignment.centerLeft,
-                  expandedCrossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("bondState : ${device.bondState.stringValue}"),
-                    Text("isBonded : ${device.isBonded}"),
-                    Text("isConnected : ${device.isConnected}"),
-                  ],
+                  trailing: ElevatedButton(
+                    onPressed: () async {
+                      try {
+                        BluetoothConnection connection =
+                            await BluetoothConnection.toAddress(device.address);
+
+                        if (!connection.isConnected) return;
+                        // ignore: use_build_context_synchronously
+                        await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ConnectDevicePage(
+                                      deviceName: device.name ?? "",
+                                      bluetoothConnection: connection,
+                                    )));
+
+                        if (connection.isConnected) {
+                          connection.dispose();
+                          print("connection.dispose()");
+                        }
+                      } catch (exception) {
+                        print('Cannot connect, exception occured');
+                      }
+                    },
+                    child: const Text("Connect"),
+                  ),
                 ),
               );
             },
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ConnectDevicePage extends StatelessWidget {
+  final String deviceName;
+  final BluetoothConnection bluetoothConnection;
+
+  const ConnectDevicePage(
+      {Key? key, required this.deviceName, required this.bluetoothConnection})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(deviceName),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (bluetoothConnection.isConnected) {
+                        bluetoothConnection.output
+                            .add(utf8.encode("1") as Uint8List);
+                        await bluetoothConnection.output.allSent;
+                      }
+                    },
+                    child: const Icon(
+                      Icons.flashlight_on_outlined,
+                      size: 64.0,
+                    ),
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.symmetric(horizontal: 8.0)),
+                SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (bluetoothConnection.isConnected) {
+                        bluetoothConnection.output
+                            .add(utf8.encode("0") as Uint8List);
+                        await bluetoothConnection.output.allSent;
+                      }
+                    },
+                    child: const Icon(
+                      Icons.flashlight_off_outlined,
+                      size: 64.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Padding(padding: EdgeInsets.symmetric(vertical: 16.0)),
+            // StreamBuilder(
+            //   stream: bluetoothConnection.input,
+            //   builder: (context, snapshot) {
+            //     final String message;
+            //     if (!snapshot.hasData) {
+            //       message = "<<";
+            //     } else {
+            //       print(snapshot.data.toString());
+            //       message = ascii.decode(snapshot.data as List<int>);
+            //     }
+            //     return Container(
+            //       width: 350,
+            //       height: 400,
+            //       color: Colors.black,
+            //       child: Text(
+            //         message,
+            //         style: const TextStyle(color: Colors.white),
+            //       ),
+            //     );
+            //   },
+            // ),
+          ],
+        ),
       ),
     );
   }
